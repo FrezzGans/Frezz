542f1c5a8a753780848ed4b8d89fb87b84489c53
Frezz <49801480+FrezzGans@users.noreply.github.com>
2019-06-19 12:44:20 +0700

    F
    F

---
+++ Frezz.py
@@ -0,0 +1,37 @@
+#AuthorAndCreatorFrezz
+#FrezzGansEa
+
+import sys
+import os
+import time
+
+os.system("figlet Frezz")
+print " "
+print "======================="
+print "Author : Frezz"
+print "Team : MythSquad"
+print "Tools : Register"
+print "======================="
+print " "
+login = input("Login-Y/N#  ")
+jawab = raw_input("Okee#  ")
+print "LOADING...."
+print "Tunggu 10 Detik"
+time.sleep(5)
+os.system("figlet Frezz")
+time.sleep(5)
+print "[Done]"
+os.system("clear")
+print "======================="
+print "Author : Frezz"
+print "Team : MythSquad"
+print "Tools : Register"
+print "======================="
+print " "
+nama = input("Masukan Nama Anda@")
+password = raw_input("MasukanPassword@")
+print " "
+time.sleep(2)
+print "BERHASIL"
+
+
